b81bbb6ad9a34cf65a336cd61d307574d48cb2b6
harle <harley.pool@my.jcu.edu.au>
2025-09-25 22:05:18 +1000

    Converted to functions for stars program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,28 @@
+"""
 MIN_LENGTH = 6
 password = input("Enter the password: ")
 while len(password) < MIN_LENGTH:
     password = input("Enter the password: ")
 print('*' * len(password))
+"""
+
+MIN_LENGTH = 6
+
+
+def main():
+    password = get_password()
+    asterisks(password)
+
+
+def asterisks(password):
+    print('*' * len(password))
+
+
+def get_password():
+    password = input("Enter the password: ")
+    while len(password) < MIN_LENGTH:
+        password = input("Enter the password: ")
+    return password
+
+
+main()
