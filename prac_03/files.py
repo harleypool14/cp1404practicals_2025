c026d1f2f0757a129a6fe2f0a1623cd29abb33a3
harle <harley.pool@my.jcu.edu.au>
2025-10-04 23:39:24 +1000

    Complete questions for files program and added docstrings

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,12 +1,18 @@
 # Write code that asks the user for their name, then opens a file called name.txt and writes that name to it. Use
 # open and close for this question.
-
-
+"""Ask user for name and writes to file"""
+out_file = open("name.txt", "w")
+name = input("What is your name? ")
+print(name, file=out_file)
+out_file.close()
 # In the same file, but as if it were a separate program, write code that opens "name.txt" and reads the name (as
 # above) then prints (note the exact output), Hi Bob! (or whatever the name is in the file). Do not simply print the
 # user's input variable! Use open and close for this question.
-
-
+"""Reads the stored name from file"""
+in_file = open("name.txt", "r")
+name = in_file.read().strip()
+in_file.close()
+print(f"Hi {name}!")
 # Create a text file called numbers.txt and save it in your prac directory.
 # Put the following three numbers on separate lines in the file and save it:
 # 17
@@ -14,8 +20,17 @@
 # 400
 # Write code that opens numbers.txt, reads only the first two numbers, adds them
 # together then prints the result, which should be... 59. Use with instead of open and close for this question.
-
-
+"""Opens number file and reads two numbers and adds them together"""
+with open("numbers.txt", "r") as in_file:
+    number1 = int(in_file.readline())
+    number2 = int(in_file.readline())
+print(number1 + number2)
 # Now write a fourth block of code that prints the total for all lines in numbers.txt.
 # This should work for a file with any number of numbers. Use with instead of open and close for this question.
-
+"""Open and reads the numbers from the file"""
+total = 0
+with open("numbers.txt", "r") as in_file:
+    for line in in_file:
+        number = int(line)
+        total += number
+print(total)
