e195c34badd6e28f46dccf8009bfecc29097808c
harle <harley.pool@my.jcu.edu.au>
2025-10-05 19:25:06 +1000

    Finish random number program

---
+++ prac_03/randoms.py
@@ -0,0 +1,28 @@
+"""
+What did you see on line 1?
+I saw random number coming up ranging from 5 - 20.
+What was the smallest number you could have seen, what was the largest?
+The smallest number that i could have seen was 5 and the largest was 20.
+
+What did you see on line 2?
+I saw random numbers coming up going up and down by 2 starting from 3 going to 5, 7 and 9
+What was the smallest number you could have seen, what was the largest?
+The smallest number that i could have seen was 3 and the largest was 9.
+
+Could line 2 have produced a 4?
+No it could not have produced 4 as it was going up in 2's starting from 3.
+
+What did you see on line 3?
+After entering the command into the python console I was able to see that
+there were random numbers with a bunch of decimals ranging from 2.5 - 5.5.
+What was the smallest number you could have seen, what was the largest?
+The smallest number I could have seen was 2.5 and the largest was 5.5.
+"""
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive
+
+import random
+
+print(random.randint(1, 100))
+
+
