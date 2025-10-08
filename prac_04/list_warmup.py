b1e4b33e556695c98a2d6f3799f5def7431173d7
harle <harley.pool@my.jcu.edu.au>
2025-10-08 23:58:44 +1000

    Finish list warmup

---
+++ prac_04/list_warmup.py
@@ -0,0 +1,25 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+"""
+numbers[0] = 3
+numbers[-1] = 2
+numbers[3] = 1
+numbers[:-1] =  [3,1,4,1,5,9]
+numbers[3:4] = [3,1,4]
+5 in numbers = True
+7 in numbers = False (7 is not in the list)
+"3" in numbers = False (no string)
+numbers + [6, 5, 3] = [3,1,4,1,5,9,2,6,5,3]
+"""
+
+"""
+Change the first element of numbers to "ten" (the string, not the number 10)
+Answer = numbers[0] = "ten
+Change the last element of numbers to 1
+Answer = numbers[-1] = 1
+Print all the elements from numbers except the first two (slice)
+Answer = print(numbers[2:1])
+Print whether 9 is an element of numbers
+print(9 in numbers)
+"""
+
