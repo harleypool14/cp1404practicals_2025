6cc811bcf5d78554a2fa98fde0f0f625a3d8fc89
harle <harley.pool@my.jcu.edu.au>
2025-10-08 23:13:29 +1000

    Added f strings to income and last print message

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -10,7 +10,7 @@ def main():
     number_of_months = int(input("How many months? "))
 
     for month in range(1, number_of_months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+        income = float(input(f"Enter income for month {month}"))
         incomes.append(income)
 
     income_report(incomes)
@@ -22,7 +22,7 @@ def income_report(incomes):
     for month, income in enumerate(incomes, 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}".format(month, income, total))
 
 
 main()
