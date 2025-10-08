a3fc44fa63cc213c23f6239304bbe795a5ebc2f2
harle <harley.pool@my.jcu.edu.au>
2025-10-08 23:40:46 +1000

    Add f strings to print statistics for list of numbers

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -2,3 +2,9 @@ numbers = []
 for i in range(5):
     num = input(input(f"Number {1 + i}:"))
     numbers.append(num)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[4]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The Largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
