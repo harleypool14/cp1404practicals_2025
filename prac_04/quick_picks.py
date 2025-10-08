7bdca4ff66c3f41fa41781b95a41eb5a07a45ac6
harle <harley.pool@my.jcu.edu.au>
2025-10-09 00:17:59 +1000

    Add while loop to check and sort for quick pick numbers

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -15,7 +15,11 @@ def main():
         quick_pick = []
         for n in range(NUMBER_PER_LINE):
             number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
-
+            while number in quick_pick:
+                number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+            quick_pick.append(number)
+        quick_pick.sort()
+        print(" ".join(f"{number:2}" for number in quick_pick))
 
 
 main()
