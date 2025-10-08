89bd07a203540a211a589b474661786f780ef824
harle <harley.pool@my.jcu.edu.au>
2025-10-08 23:52:19 +1000

    Add user authentication for the list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -8,3 +8,10 @@ print(f"The smallest number is {min(numbers)}")
 print(f"The Largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter user name: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
