f7c39617e4296cf505bb8ca3e691bb9a7bd220e8
harle <harley.pool@my.jcu.edu.au>
2025-10-15 22:55:27 +1000

    Add while loop so user can enter name and get code

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,3 +1,7 @@
 COLOUR_CODES = {"aliceblue": "#f0f8ff", "absolutezero": "#0048ba", "darkorchid": "#9932cc", "forestgreen": "#228b22",
                 "goldenrod": "#daa520", "lavender": "#e6e6fa", "maroon": "#800000", "navy": "#000080",
                 "plum": "#dda0dd", "amber": "#ffbf00"}
+colour = input("Enter a colour name: ").lower()
+while colour != "":
+    print(f"The code for {colour} is {COLOUR_CODES.get(colour)}")
+    colour = input("Enter a colour name: ").lower()
