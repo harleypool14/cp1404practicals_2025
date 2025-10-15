36f9090a68a6fe1f95e69d6ad1a8d37ca3e61f37
harle <harley.pool@my.jcu.edu.au>
2025-10-15 22:18:02 +1000

    Fixed to use exceptions instead of an if statement

--- prac_05/states_names.py
+++ prac_05/states_names.py
@@ -4,7 +4,6 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 for state_code, name in CODE_TO_NAME.items():
@@ -12,8 +11,8 @@ for state_code, name in CODE_TO_NAME.items():
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
