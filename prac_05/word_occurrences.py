e742d61a132f08c21c5bb8382c581fd7abc1d2ea
harle <harley.pool@my.jcu.edu.au>
2025-10-15 23:18:56 +1000

    Add loop to display words in alphabetical order

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -10,4 +10,6 @@ words = text.split()
 for word in words:
     frequency = word_count.get(word, 0)
     word_count[word] = frequency + 1
+
+for word in sorted(word_count):
     print(f"{word:} : {word_count[word]}")
