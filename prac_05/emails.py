4a34d9f6a79fbb85e7766c7921020df5ad5a563f
harle <harley.pool@my.jcu.edu.au>
2025-10-15 23:49:17 +1000

    Add for loop and extract name function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -9,11 +9,19 @@ def main():
     email_to_name = {}
     email = input("Email: ")
     while email != "":
+        name = extract_name(email)
         confirm = input(f"Is your name {name}? (Y/n) ").lower()
         if confirm.upper() != "Y" and confirm != "":
             name = input("Name: ")
         email_to_name[email] = name
         email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def extract_name(email):
+    return name
 
 
 main()
