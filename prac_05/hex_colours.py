47b7c0d06357fe072f3cc268d3cdf8fbe3a8f340
harle <harley.pool@my.jcu.edu.au>
2025-10-15 22:48:30 +1000

    Add colour codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+COLOUR_CODES = {"aliceblue": "#f0f8ff", "absolutezero": "#0048ba", "darkorchid": "#9932cc", "forestgreen": "#228b22",
+                "goldenrod": "#daa520", "lavender": "#e6e6fa", "maroon": "#800000", "navy": "#000080",
+                "plum": "#dda0dd", "amber": "#ffbf00"}
