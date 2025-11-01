ceaa5997c1bf8bc793d22ad0e65eb6c8faf1a5db
harle <harley.pool@my.jcu.edu.au>
2025-11-02 00:31:31 +1000

    Add loop to display dynamically typed languages

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,7 +1,13 @@
 from programming_languages import ProgrammingLanguage
 
+
 def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    print(python)
+
+    languages = [python, ruby, visual_basic]
+    print("\nThe dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
