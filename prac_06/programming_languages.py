3fcf5bc839e0879d282f02504d8950b3714a604d
harle <harley.pool@my.jcu.edu.au>
2025-11-02 00:05:42 +1000

    Create programming language class

---
+++ prac_06/programming_languages.py
@@ -0,0 +1,14 @@
+"""
+estimate: 35 minutes
+time taken:  minutes
+"""
+
+
+class ProgrammingLanguage:
+    def __init__(self, name="", typing="", reflection="", year=""):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
