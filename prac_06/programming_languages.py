506acf151a550abbb2844df0ccc272f35ad7bbb2
harle <harley.pool@my.jcu.edu.au>
2025-11-02 00:32:28 +1000

    Add run_test function test programming language class

--- prac_06/programming_languages.py
+++ prac_06/programming_languages.py
@@ -18,3 +18,18 @@ class ProgrammingLanguage:
         return f"{self.name}, {self.typing}, Typing, Reflection={self.reflection}, First appeard in {self.year}"
 
 
+def run_tests():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [ruby, python, visual_basic]
+    print(python)
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == "__main__":
+    run_tests()
