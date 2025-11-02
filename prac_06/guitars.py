66d859712b9c97714ce26f48556a231acc5aa36a
harle <harley.pool@my.jcu.edu.au>
2025-11-02 18:50:36 +1000

    Add code to display all guitars with vintage status

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -14,3 +14,16 @@ def main():
         name = input("Name: ")
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    # Display all guitars
+    if guitars:
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = " (vintage)"
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+if __name__ == '__main__':
+    main()
