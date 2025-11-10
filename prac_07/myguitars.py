a1945a877b2915af2ecff89cdce77aaac4a8ba92
harle <harley.pool@my.jcu.edu.au>
2025-11-10 21:47:42 +1000

    Add main function with sort and save features

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,17 +2,15 @@ from guitar import Guitar
 
 
 def main():
-    """Main program for guitar collection management."""
     filename = 'guitars.csv'
     guitars = load_guitars(filename)
     print("Loaded guitars:")
     display_guitars(guitars)
-    # Add the __lt__ method to the Guitar class for sorting by year
-    Guitar.__lt__ = lambda self, other: self.year < other.year
+
     print("\nSorted by year:")
     guitars.sort()
     display_guitars(guitars)
-    # Get new guitars from user
+
     print("\nAdd new guitars:")
     name = input("Name: ")
     while name != "":
@@ -25,26 +23,28 @@ def main():
         except ValueError:
             print("Invalid input. Please enter valid numbers for year and cost.")
         name = input("Name: ")
-        # Save all guitars back to file
-        save_guitars(guitars, filename)
-        print("\nGuitars have been saved to", filename)
-        print("\nFinal guitar collection:")
-        display_guitars(guitars)
+
+    save_guitars(guitars, filename)
+    print("\nGuitars have been saved to", filename)
+    print("\nFinal guitar collection:")
+    display_guitars(guitars)
 
 
-def load_guitars(filename):
+def load_guitars():
+    return
 
 
-
-def save_guitars(guitars, filename):
-
+def save_guitars():
+    return
 
 
 def get_new_guitar():
+    return
 
 
+def display_guitars():
+    return
 
-def display_guitars(guitars):
 
 if __name__ == '__main__':
     main()
