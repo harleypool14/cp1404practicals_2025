0a066c16cef9893bddd897cc75fe3c375ddc45eb
harle <harley.pool@my.jcu.edu.au>
2025-11-16 23:55:35 +1000

    Add dynamic label creation in build

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -9,3 +9,15 @@ class DynamicLabelsApp(App):
         """Construct main app"""
         super().__init__(**kwargs)
         self.names = ["Bob Brown", "Cat Cyan", "Oren Ochre", "Pete Purple"]
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
+        return self.root
+
+
+DynamicLabelsApp().run()
