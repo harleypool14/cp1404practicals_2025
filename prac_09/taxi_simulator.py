33b601b26a402a6a92a300d74ab729c9f750666e
harle <harley.pool@my.jcu.edu.au>
2025-11-23 23:52:00 +1000

    Start main function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,4 +3,14 @@ from silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
 
+def main():
+    """Taxi simulator program using Taxi and SilverServiceTaxi classes"""
+    taxis = [
+        Taxi("Prius", 100),
+        SilverServiceTaxi("Limo", 100, 2),
+        SilverServiceTaxi("Hummer", 200, 4)
+    ]
+    taxi_bill = 0.0
+    current_taxi = None
+
 
