e0ca753842c1df6a517b8d5f5f9037dc71e86dfe
harle <harley.pool@my.jcu.edu.au>
2025-11-23 23:42:23 +1000

    Add band class

---
+++ prac_09/band.py
@@ -0,0 +1,13 @@
+class Band:
+    def __init__(self, name=""):
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        pass
+
+    def add(self, musician):
+        pass
+
+    def play(self):
+        pass
