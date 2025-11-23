9c10083ee5b1bed1e69523a217760d38ffda86a7
harle <harley.pool@my.jcu.edu.au>
2025-11-23 23:52:52 +1000

    Add main while loop to taxi simulator

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -13,4 +13,32 @@ def main():
     taxi_bill = 0.0
     current_taxi = None
 
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").lower()
 
+    while choice != "q":
+        if choice == "c":
+            print("Taxis available:")
+            display_taxis(taxis)
+            try:
+                choose_taxi = int(input("Choose taxi: "))
+                current_taxi = taxis[choose_taxi]
+            except (IndexError, ValueError):
+                print("Invalid taxi choice")
+        elif choice == "d":
+            if current_taxi:
+                current_taxi.start_fare()
+                drive_distance = float(input("Drive how far? "))
+                current_taxi.drive(drive_distance)
+                trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
+                taxi_bill += trip_cost
+            else:
+                print("You need to choose a taxi before you can drive")
+        else:
+            print("Invalid option")
+
+        print(f"Bill to date: ${taxi_bill:.2f}")
+        print(MENU)
+        choice = input(">>> ").lower
