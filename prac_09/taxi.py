f42be0b02786b8b8a304a433f12df5439bd008c1
harle <harley.pool@my.jcu.edu.au>
2025-11-23 20:13:33 +1000

    Add class variable to taxi

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -2,11 +2,12 @@
 CP1404/CP5632 Practical
 Car class
 """
-from prac_09.car import Car
+from car import Car
 
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
+    price_per_km = 1.23
 
     def __init__(self, name, fuel, price_per_km):
         """Initialise a Taxi instance, based on parent class Car."""
