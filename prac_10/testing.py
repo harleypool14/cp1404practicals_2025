1797ff3157e255ab76a96a7b36f3787c4ac937e1
harle <harley.pool@my.jcu.edu.au>
2025-12-01 12:16:32 +1000

    Finish all ToDo's

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -32,8 +32,9 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
     # Hint: "-".join(["yo", "yo"] -> "yo-yo"
+    def repeat_string(s, n):
+        return "-".join([s] * n)
 
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
@@ -46,18 +47,23 @@ def run_tests():
     # using the value passed in or the default
     # You should test both of these
     car = Car(fuel=10)
+    assert car._odometer == 0, "Car does not set odometer correctly"
+
+    car_default = Car()
+    assert car_default.fuel == Car.DEFAULT_FUEL
 
 
 run_tests()
 
-# TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
+doctest.testmod()
 
-# TODO: 4. Fix the failing is_long_word function
+
 # (Don't change the tests, change the function!)
+def is_long_word(word, length=5):
+    return len(word) >= length
 
-# TODO: 5. Write and test a function to format a phrase as a sentence,
+
 # starting with a capital and ending with a single full stop.
 # Important: start with a function header and just use pass as the body
 # then add doctests for 3 tests:
@@ -66,3 +72,5 @@ run_tests()
 # and one more that you decide is a useful test.
 # Run your doctests and watch the tests fail.
 # Then write the body of the function so that the tests pass.
+def format_sentence(phrase):
+    return phrase.capitalize().rstrip('.') + '.'
