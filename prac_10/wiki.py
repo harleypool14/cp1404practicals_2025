ff8ec4b4588000adc46a219d886749d94cf70f11
harle <harley.pool@my.jcu.edu.au>
2025-12-01 12:26:49 +1000

    Add error handling for missing Wikipedia pages

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,5 +1,6 @@
 import wikipedia
 
+
 def main():
     search_title = input("Enter page title: ")
     while search_title:
@@ -8,3 +9,17 @@ def main():
             print(page.title)
             print(page.summary)
             print(page.url)
+
+        except wikipedia.Error as e:
+            print(f"We need a more specific title. Try one of the following, or a new search:")
+            print(e.options)
+
+        except wikipedia.PageError:
+            print(f"Page id \"{search_title}\" does not match any pages. Try another id!")
+        print()
+        search_title = input("Enter page title: ")
+    print("Thank you.")
+
+
+if __name__ == "__main__":
+    main()
